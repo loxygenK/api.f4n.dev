d612d67d26211523ca5eb2f032811748aafc3eb5
loxygenk <me@loxygen.dev>
2021-09-20 22:15:42 +0900

    ✨ Define 'new' function

--- src/domain/basic.rs
+++ src/domain/basic.rs
@@ -5,17 +5,40 @@ pub struct Basic {
     name: Name,
     introduction: String,
     affiliation: Vec<Affiliation>,
-    age: i32
+    age: i32,
+}
+
+impl Basic {
+    pub fn new(name: Name, introduction: String, affiliation: Vec<Affiliation>, age: i32) -> Self {
+        Self {
+            name,
+            introduction,
+            affiliation,
+            age,
+        }
+    }
 }
 
 #[derive(GraphQLObject)]
 pub struct Name {
     primary: String,
-    aka: Vec<String>
+    aka: Vec<String>,
+}
+
+impl Name {
+    pub fn new(primary: String, aka: Vec<String>) -> Self {
+        Self { primary, aka }
+    }
 }
 
 #[derive(GraphQLObject)]
 pub struct Affiliation {
     location: String,
-    assign: String
+    assign: String,
+}
+
+impl Affiliation {
+    pub fn new(location: String, assign: String) -> Self {
+        Self { location, assign }
+    }
 }
