560283581aa319240b83142d2ec09e0ec7fbd759
loxygenk <me@loxygen.dev>
2021-09-27 18:53:52 +0900

    ♻ Unify all Query definition into the one impl

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,10 +1,15 @@
-use juniper::{EmptyMutation, EmptySubscription};
+use juniper::{EmptyMutation, EmptySubscription, FieldResult};
+
+use crate::domain::{
+    basic::{Affiliation, Basic, Name},
+    career::Career,
+    contact::Contact,
+    skill::{Skill, SkillType, SkilledLevel},
+    work::{Status, Work},
+};
 
 use self::state::State;
 
-pub struct QueryRoot;
-
-pub mod basic;
 pub mod state;
 
 pub type GraphQLScheme =
@@ -16,4 +21,58 @@ pub fn generate_scheme() -> GraphQLScheme {
         EmptyMutation::<State>::new(),
         EmptySubscription::<State>::new(),
     )
+}
+
+pub struct QueryRoot;
+
+#[juniper::graphql_object(context = State)]
+impl QueryRoot {
+    fn basic(_state: &State) -> FieldResult<Basic> {
+        Ok(Basic::new(
+            Name::new("loxygen.k".to_string(), vec!["Flisan".to_string()]),
+            "Hello, I'm Flisan!".to_string(),
+            vec![Affiliation::new(
+                "NITIC".to_string(),
+                "3rd grade, Information dep.".to_string(),
+            )],
+            18,
+        ))
+    }
+
+    fn careers(_state: &State) -> FieldResult<Vec<Career>> {
+        Ok(vec![Career::new(
+            chrono::Utc::now(),
+            "whatever".to_string(),
+            None,
+            None,
+        )])
+    }
+
+    fn contacts(_state: &State) -> FieldResult<Vec<Contact>> {
+        Ok(vec![Contact::new(
+            "Twitter".to_string(),
+            "loxygenK".to_string(),
+            "https://twitter.com/loxygen_k".to_string(),
+        )])
+    }
+
+    fn skills(_state: &State) -> FieldResult<Vec<Skill>> {
+        Ok(vec![Skill::new(
+            "Something".to_string(),
+            SkillType::Miscellaneous,
+            SkilledLevel::Advanced,
+            "whatever".to_string(),
+        )])
+    }
+
+    fn works(_state: &State) -> FieldResult<Vec<Work>> {
+        Ok(vec![Work::new(
+            "whatever".to_string(),
+            "hoge".to_string(),
+            Some("repo".to_string()),
+            None,
+            vec!["what".to_string(), "ever".to_string()],
+            Status::Advancing,
+        )])
+    }
 }
