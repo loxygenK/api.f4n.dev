dd194823b9d56b26084fb54df5e2f606ad9663c9
loxygenk <me@loxygen.dev>
2021-09-27 21:32:48 +0900

    ✨ Implement AssetRepository

--- src/repository/mock.rs
+++ src/repository/mock.rs
@@ -1,27 +1,27 @@
 use crate::domain::{basic::Basic, career::Career, contact::Contact, skill::Skill, work::Work};
 
-use super::{Repository, RepositoryResult};
+use super::{Repository, RepositoryResult, util::serialize_yaml};
 
 struct AssetRepository;
 
 impl Repository for AssetRepository {
     fn fetch_basic() -> RepositoryResult<Basic> {
-        todo!()
+        serialize_yaml("asset/basic.yaml")
     }
 
     fn fetch_careers() -> RepositoryResult<Vec<Career>> {
-        todo!()
+        serialize_yaml("asset/career.yaml")
     }
 
     fn fetch_contacts() -> RepositoryResult<Vec<Contact>> {
-        todo!()
+        serialize_yaml("asset/content.yaml")
     }
 
     fn fetch_skills() -> RepositoryResult<Vec<Skill>> {
-        todo!()
+        serialize_yaml("asset/skill.yaml")
     }
 
     fn fetch_works() -> RepositoryResult<Vec<Work>> {
-        todo!()
+        serialize_yaml("asset/work.yaml")
     }
 }
