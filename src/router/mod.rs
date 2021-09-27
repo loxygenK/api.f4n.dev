a4794b5d00d6310bbfeadb2b264f369bce979b3b
loxygenk <me@loxygen.dev>
2021-09-27 23:18:20 +0900

    ✨ Implement Query using injected service

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -1,6 +1,6 @@
 pub mod state;
 
-use juniper::{EmptyMutation, EmptySubscription, FieldResult};
+use juniper::{EmptyMutation, EmptySubscription, FieldError, FieldResult};
 
 use crate::{
     domain::{
@@ -24,53 +24,34 @@ pub struct Query {
 
 #[juniper::graphql_object(context = State)]
 impl Query {
-    fn basic(_state: &State) -> FieldResult<Basic> {
-        Ok(Basic::new(
-            Name::new("loxygen.k".to_string(), vec!["Flisan".to_string()]),
-            "Hello, I'm Flisan!".to_string(),
-            vec![Affiliation::new(
-                "NITIC".to_string(),
-                "3rd grade, Information dep.".to_string(),
-            )],
-            18,
-        ))
+    fn basic(&self, _state: &State) -> FieldResult<Basic> {
+        self.service
+            .fetch_basic()
+            .map_err(|e| FieldError::new(e, juniper::Value::Null))
     }
 
-    fn careers(_state: &State) -> FieldResult<Vec<Career>> {
-        Ok(vec![Career::new(
-            chrono::Utc::now(),
-            "whatever".to_string(),
-            None,
-            None,
-        )])
+    fn careers(&self, _state: &State) -> FieldResult<Vec<Career>> {
+        self.service
+            .fetch_careers()
+            .map_err(|e| FieldError::new(e, juniper::Value::Null))
     }
 
-    fn contacts(_state: &State) -> FieldResult<Vec<Contact>> {
-        Ok(vec![Contact::new(
-            "Twitter".to_string(),
-            "loxygenK".to_string(),
-            "https://twitter.com/loxygen_k".to_string(),
-        )])
+    fn contacts(&self, _state: &State) -> FieldResult<Vec<Contact>> {
+        self.service
+            .fetch_contacts()
+            .map_err(|e| FieldError::new(e, juniper::Value::Null))
     }
 
-    fn skills(_state: &State) -> FieldResult<Vec<Skill>> {
-        Ok(vec![Skill::new(
-            "Something".to_string(),
-            SkillType::Miscellaneous,
-            SkilledLevel::Advanced,
-            "whatever".to_string(),
-        )])
+    fn skills(&self, _state: &State) -> FieldResult<Vec<Skill>> {
+        self.service
+            .fetch_skills()
+            .map_err(|e| FieldError::new(e, juniper::Value::Null))
     }
 
-    fn works(_state: &State) -> FieldResult<Vec<Work>> {
-        Ok(vec![Work::new(
-            "whatever".to_string(),
-            "hoge".to_string(),
-            Some("repo".to_string()),
-            None,
-            vec!["what".to_string(), "ever".to_string()],
-            Status::Advancing,
-        )])
+    fn works(&self, _state: &State) -> FieldResult<Vec<Work>> {
+        self.service
+            .fetch_works()
+            .map_err(|e| FieldError::new(e, juniper::Value::Null))
     }
 }
 
