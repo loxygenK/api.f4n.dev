140f2c0248312b735a5897a970766977b14ecc3f
loxygenk <me@loxygen.dev>
2021-10-30 18:00:08 +0900

    ✨ Implement retrieving from loxygenk.d

--- src/repository/loxygenk_d.rs
+++ src/repository/loxygenk_d.rs
@@ -15,33 +15,31 @@ const LOXYGENK_D_PATH: &str = "https://raw.githubusercontent.com/loxygenK/loxyge
 pub struct LoxygenKDRepository;
 impl Repository for LoxygenKDRepository {
     fn fetch_basic(&self) -> super::RepositoryResult<Basic> {
-        todo!()
+        retrieve_from("basic.yaml")
     }
 
     fn fetch_careers(&self) -> super::RepositoryResult<Vec<Career>> {
-        todo!()
+        retrieve_from("career.yaml")
     }
 
     fn fetch_contacts(&self) -> super::RepositoryResult<Vec<Contact>> {
-        todo!()
+        retrieve_from("contacts.yaml")
     }
 
     fn fetch_skills(&self) -> super::RepositoryResult<Vec<Skill>> {
-        todo!()
+        retrieve_from("skills.yaml")
     }
 
     fn fetch_works(&self) -> super::RepositoryResult<Vec<Work>> {
-        todo!()
+        retrieve_from("works.yaml")
     }
 }
 
-fn retrieve(path: &str) -> RepositoryResult<String> {
-    reqwest::blocking::get(format!("{}/{}", LOXYGENK_D_PATH, path))
+fn retrieve_from<T: DeserializeOwned>(path: &str) -> RepositoryResult<T> {
+    let content = reqwest::blocking::get(format!("{}/{}", LOXYGENK_D_PATH, path))
         .map_err(|e| RepositoryError::RetrievingError(Box::new(e)))?
         .text()
-        .map_err(|e| RepositoryError::RetrievingError(Box::new(e)))
-}
+        .map_err(|e| RepositoryError::RetrievingError(Box::new(e)))?;
 
-fn serialize_yaml<T: DeserializeOwned>(content: &str) -> RepositoryResult<T> {
-    serde_yaml::from_str(content).map_err(|e| RepositoryError::DeserializationError(e))
+    serde_yaml::from_str(&content).map_err(|e| RepositoryError::DeserializationError(e))
 }
